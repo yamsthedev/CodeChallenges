43e0527bd9edd6d2ad3a54f87d0e9b66d1fa52db
Sam Luong <newaccrs@gmail.com>
2022-04-04 20:57:48 -0400

    Add 2022-04-04 Solutions

---
+++ 8 Kyu/2022-04-04-Problem.js
@@ -0,0 +1,24 @@
+//8 Kyu Problem - April 4th, 2022
+//DNA to RNA Conversion
+
+// Problem Description:
+
+// Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+// Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+// Create a function which translates a given DNA string into RNA.
+
+// For example:
+
+// "GCAT"  =>  "GCAU"
+// The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+function DNAtoRNA(dna){
+    return dna.replaceAll("T", "U")
+}
+
+//need node 15.0.0 for this to work however.
+//Alternative solution would be:
+
+//return dna.split("T").join("U");
