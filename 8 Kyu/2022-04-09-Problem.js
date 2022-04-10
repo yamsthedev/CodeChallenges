97afb5ca8ce4418d8ffcd988caa1f9470f19fa92
Sam Luong <newaccrs@gmail.com>
2022-04-09 22:46:37 -0400

    add two solutions to code problems for the day

---
+++ 8 Kyu/2022-04-09-Problem.js
@@ -0,0 +1,16 @@
+// 8 Kyu Problem - April 9th, 2022
+// Convert a string to an array
+
+//Description:
+// Write a function to split a string and convert it into an array of words.
+
+// Examples (Input -> Output):
+// * "Robin Singh" ==> ["Robin", "Singh"]
+
+// * "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+function stringToArray(string){
+    return string.split(' ' );
+      // code code code
+    
+    }
