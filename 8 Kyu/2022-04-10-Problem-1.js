891f8d2d874afd0843b259d04682cb9d4189c6bb
Sam Luong <newaccrs@gmail.com>
2022-04-10 20:13:59 -0400

    Code Challenges - April 10th 2022 - 8 Kyu Problem - Messi goals function

---
+++ 8 Kyu/2022-04-10-Problem-1.js
@@ -0,0 +1,22 @@
+// 8 Kyu Problem - April 10th 2022
+// Messi goals function
+
+// Messi is a soccer player with goals in three leagues:
+
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+// For example:
+
+// 5, 10, 2  -->  17
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  }
+
+// Alternative Solution
+const goals = (...a) => a.reduce((s, v) => s + v, 0);
