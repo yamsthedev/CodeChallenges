34503be99e127510a833cb709744ce62c2b61fa5
Sam Luong <newaccrs@gmail.com>
2022-04-12 22:33:00 -0400

    daily coding challenge for april 12

---
+++ 8 Kyu/2022-04-12-Problem.js
@@ -0,0 +1,17 @@
+// 8 Kyu Problem - April 12th, 2022
+// Simple Multiplication
+
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+// function simpleMultiplication(number) {
+//     // your code........
+//   if(number%2 === 0){
+//     return number*8
+//   }else{
+//     return number*9
+//   }
+// }
+
+function simpleMultiplication(number){
+    return number%2 === 0 ? number*8 : number * 9;
+  }
