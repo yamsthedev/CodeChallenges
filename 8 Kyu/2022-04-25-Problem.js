13f1b42dd39c8450e44a92a95de5360598d3435f
Sam Luong <newaccrs@gmail.com>
2022-04-25 22:32:20 -0400

    Code Challenge - April 25, 2022 - One 8 Kyu Problem

---
+++ 8 Kyu/2022-04-25-Problem.js
@@ -0,0 +1,47 @@
+// 8 Kyu Problem - April 25, 2022
+// Rock Paper Scissors!
+
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples:
+
+// rps('scissors','paper') // Player 1 won!
+// rps('scissors','rock') // Player 2 won!
+// rps('paper','paper') // Draw!
+
+const rps = (p1, p2) => {
+    if (p1 === "rock" && p2 === "scissors" || p1 === "scissors" && p2 === "paper" || p1 === "paper" && p2 === "rock") {
+      return "Player 1 won!";
+    } else if (p2 === "rock" && p1 === "scissors" || p2 === "scissors" && p1 === "paper" || p2 === "paper" && p1 === "rock") {
+      return "Player 2 won!";
+    } else {
+      return "Draw!";
+    }
+  };
+
+//Alternative Solution
+const rps = (p1, p2) => {
+    if (p1 === p2) return "Draw!";
+    var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
+    if (p2 === rules[p1]) {
+      return "Player 1 won!";
+    }
+    else {
+      return "Player 2 won!";
+    }
+  };
+
+//Alternative Solution
+const rps = (p1, p2) => {
+    if (p1 == p2)
+      return 'Draw!';
+      
+     if (p1 == 'rock' && p2 == 'scissors') 
+       return 'Player 1 won!'
+     else if (p1 == 'scissors' && p2 == 'paper') 
+       return 'Player 1 won!'
+     else if (p1 == 'paper' && p2 == 'rock') 
+       return 'Player 1 won!'
+     else
+       return 'Player 2 won!';
+  };
