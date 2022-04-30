bebad77ab7412e7d2ed85b83e31da935ba9f1d3c
Sam Luong <newaccrs@gmail.com>
2022-04-30 15:12:07 -0400

    Code Challenges - April 30th, 2022 - 7 Kyu Problem and 8 Kyu problem

---
+++ 7_kyu/2022-04-30-Problem2.js
@@ -0,0 +1,20 @@
+// 7 Kyu Problem - April 30th, 2022
+// 2nd Problem of the day
+// Two to One
+
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+function longest(s1, s2) {
+    return Array.from(new Set(s1 + s2)).sort().join('');
+  }
+
+// Alternative Solution
+const longest = (s1, s2) => [...new Set(s1+s2)].sort().join('')
