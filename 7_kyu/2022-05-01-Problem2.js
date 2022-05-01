860324f2bd15c6bcedb98e3034e26085beced81f
Sam Luong <newaccrs@gmail.com>
2022-05-01 00:16:57 -0400

    Code Challenges - May 1st 2022 - 8 Kyu Problem and 7 Kyu Problem

---
+++ 7_kyu/2022-05-01-Problem2.js
@@ -0,0 +1,34 @@
+// 7 Kyu Problem - May 1st 2022
+// 2nd problem of the day
+// Find the next perfect square!
+
+// You might know some pretty large perfect squares. But what about the NEXT one?
+
+// Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+// If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+
+// Examples:(Input --> Output)
+
+// 121 --> 144
+// 625 --> 676
+// 114 --> -1 since 114 is not a perfect square
+
+function findNextSquare(sq) {
+    // Return the next square if sq is a perfect square, -1 otherwise
+    return Number.isInteger(Math.sqrt(sq)) ? Math.pow((Math.sqrt(sq)+1),2) : -1;
+  }
+
+//Alternative Solution
+function findNextSquare(sq) {
+    return Math.sqrt(sq)%1? -1 : Math.pow(Math.sqrt(sq)+1,2);
+}
+
+//Alternative Solution
+function findNextSquare(sq) {
+    var number = Math.sqrt(sq);
+    if(Math.round(number) === number) {
+      return Math.pow(++number, 2)
+    }
+    return -1;
+  }
