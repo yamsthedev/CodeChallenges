d8990e9270d20f3ced078831eb4a14ba6349f26c
Sam Luong <newaccrs@gmail.com>
2022-05-06 23:05:34 -0400

    Code Challenges - 7 Kyu Problem - May 6 2022

---
+++ 7_kyu/2022-05-06-Problem.js
@@ -0,0 +1,32 @@
+// 7 Kyu Problem - May 6th, 2022
+// Odd or Even?
+
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+// Examples:
+// Input: [0]
+// Output: "even"
+
+// Input: [0, 1, 4]
+// Output: "odd"
+
+// Input: [0, -1, -5]
+// Output: "even"
+
+function oddOrEven(array) {
+    return array.reduce((acc, curr) => acc + curr, 0) % 2 === 0 ? 'even' : 'odd';
+  }
+
+// Alternative Solution
+function oddOrEven(array) {
+    var result = 0;
+    for (var i = 0; i < array.length; i++)
+    {result+=array[i];}
+    if (result%2 == 0)
+    {return "even";}
+    else{return "odd";}
+  }
