8c7096c44c436c19b2d7c9f67d4dccc456057c38
Sam Luong <newaccrs@gmail.com>
2022-05-15 17:56:26 -0400

    Code Challenges - May 15th 2022 - 7 Kyu Problem

---
+++ 7_kyu/2022-05-15-Problem.js
@@ -0,0 +1,47 @@
+// 7 Kyu Problem - May 15th 2022
+// Money, Money, Money
+
+// Mr. Scrooge has a sum of money 'P' that he wants to invest. Before he does, he wants to know how many years 'Y' this sum 'P' has to be kept in the bank in order for it to amount to a desired sum of money 'D'.
+
+// The sum is kept for 'Y' years in the bank where interest 'I' is paid yearly. After paying taxes 'T' for the year the new sum is re-invested.
+
+// Note to Tax: not the invested principal is taxed, but only the year's accrued interest
+
+// Example:
+
+//   Let P be the Principal = 1000.00      
+//   Let I be the Interest Rate = 0.05      
+//   Let T be the Tax Rate = 0.18      
+//   Let D be the Desired Sum = 1100.00
+
+
+// After 1st Year -->
+//   P = 1041.00
+// After 2nd Year -->
+//   P = 1083.86
+// After 3rd Year -->
+//   P = 1128.30
+
+function calculateYears(principal, interest, tax, desired) {
+    let P = principal, I = interest, T = tax, D = desired, Y = 0;
+    if(P===D){
+      return Y;
+    }else{
+      while(P <= D){
+      P = P + (P*I) - (P*I*T)
+      Y++
+    }
+  }
+ return Y
+}
+
+//Alternative Solution
+function calculateYears(principal, interest, tax, desired) {
+    // your code
+    var years = 0;
+    while(principal < desired){
+      principal += (principal * interest) * (1 - tax);
+      years++;
+    }
+    return years;
+}
