d185d1d33e4fdef451d7d34c76a4c5ee57498492
Sam Luong <newaccrs@gmail.com>
2022-05-21 00:06:30 -0400

    Code Challenges - May 21st 2022 - 7 Kyu Problem - Anagram Detection

---
+++ 7_kyu/2022-05-21-Problem.js
@@ -0,0 +1,24 @@
+// 7 Kyu Problem - May 21st 2022
+// Anagram Detection
+
+// An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+// Note: anagrams are case insensitive
+
+// Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+// Examples
+// "foefet" is an anagram of "toffee"
+
+// "Buckethead" is an anagram of "DeathCubeK"
+
+function isAnagram (test, original) {
+    return test.toLowerCase().split("").sort().join("") === original.toLowerCase().split("").sort().join("");
+  }
+
+// Alternative Solution
+var isAnagram = function(test, original) {
+    var t = test.toLowerCase().split('').sort().join('');
+    var o = original.toLowerCase().split('').sort().join('');
+    return (t==o)?true:false;
+  };
