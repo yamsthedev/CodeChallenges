1a2e9f2edd4a57a38622f164461eed012e06f2be
Sam Luong <newaccrs@gmail.com>
2022-05-23 12:48:05 -0400

    Code Challenges - May 23rd 2022 - 7 Kyu Problem - Remove anchor from URL

---
+++ 7_kyu/2022-05-23-Problem.js
@@ -0,0 +1,12 @@
+// 7 Kyu Problem - May 23rd 2022
+// Remove anchor from URL
+
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+function removeUrlAnchor(url){
+    return url.split('#')[0];
+}
