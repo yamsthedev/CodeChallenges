7a5aae6da3b5f11b3f6229c4cabf1d0e89ffcfe8
Sam Luong <newaccrs@gmail.com>
2022-07-12 20:05:47 -0400

    Code Challenges - July 12th 2022 - 7 Kyu Problem - Reverse a Number - -result, *-1, Math.sign(n), Math.abs(n), toString(), split(), reverse(), join(), Number(str)

---
+++ 7_kyu/2022-07-12-Problem.js
@@ -0,0 +1,34 @@
+// 7 Kyu Problem - July 12th 2022
+// Reverse a Number
+// Math.abs(), Math.sign(), split, reverse, join
+
+// Given a number, write a function to output its reverse digits. (e.g. given 123 the answer is 321)
+
+// Numbers should preserve their sign; i.e. a negative number should still be negative when reversed.
+
+// Examples
+//  123 ->  321
+// -456 -> -654
+// 1000 ->    1
+
+function reverseNumber(n) {
+    let stringedNum = n.toString().split("")
+    console.log(stringedNum)
+    if(stringedNum[0] === "-"){
+      return Number(stringedNum.slice(1).reverse().join(""))*-1
+    }else{
+      return Number(stringedNum.reverse().join(""))
+    }
+  }
+
+// Alternative Solution
+function reverseNumber(n) {
+    let isNegative = n < 0;  
+    let reverseAsString = Math.abs(n).toString().split('').reverse().join('');
+    let result = Number(reverseAsString);
+    
+    return isNegative ? -result : result;
+  }
+
+// Alternative Solution
+reverseNumber = n => (n > 0 ? 1 : -1) * Math.abs(n).toString().split('').reverse().join('')
