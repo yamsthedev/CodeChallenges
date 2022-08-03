925fc78db2ec1366a67edd43af9f076e1b2456f6
Sam Luong <newaccrs@gmail.com>
2022-08-03 19:13:25 -0400

    Code Challenges - August 3rd 2022 - 7 Kyu Problem - Higher-Order Functions Series - Greet developers - Objects, {...obj, property1:value1}

---
+++ 7_kyu/2022-08-03-Problem.js
@@ -0,0 +1,67 @@
+// 7 Kyu Problem - August 3rd 2022
+// Higher-Order Function Series - Greet developers
+// .forEach(), .map(), {...obj, key1 : value1, key2 : value2}
+
+/*
+You will be given an array of objects (associative arrays in PHP, tables in COBOL) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return an array where each object will have a new property 'greeting' with the following string value:
+
+Hi < firstName here >, what do you like the most about < language here >?
+
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+];
+your function should return the following array:
+
+[
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java',
+    greeting: 'Hi Sofia, what do you like the most about Java?'
+  },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python',
+    greeting: 'Hi Lukas, what do you like the most about Python?'
+  },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby',
+    greeting: 'Hi Madison, what do you like the most about Ruby?'
+  } 
+];
+Notes:
+
+The order of the properties in the objects does not matter (except in COBOL).
+The input array will always be valid and formatted as in the example above.
+*/
+
+function greetDevelopers(list) {
+    list.forEach(e=> {
+     e.greeting = `Hi ${e.firstName}, what do you like the most about ${e.language}?`;
+    })
+  return list;
+}
+
+// Alternative Solution
+const greetDevelopers = list => list.map(
+    dev => ({...dev, greeting: `Hi ${dev.firstName}, what do you like the most about ${dev.language}?`})
+);
+
+/*
+const circle = {
+    radius: 10
+};
+
+const coloredCircle = {
+    ...circle,
+    color: 'black'
+};
+
+console.log(coloredCircle);
+
+Output:
+{
+    radius: 10,
+    color: 'black'
+}
+*/
