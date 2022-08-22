39f083a257f5074aa0e9db408c0e45497cbe2796
Sam Luong <newaccrs@gmail.com>
2022-08-22 18:06:51 -0400

    Code Challenges - August 22nd 2022 - 7 Kyu Problem - Is Ruby Coming? - Array.prototype.some()

---
+++ 7_kyu/2022-08-22-Problem.js
@@ -0,0 +1,44 @@
+// 7 Kyu Problem - August 22nd 2022
+// Is Ruby coming?
+// Array.prototype.some(), for loop, .forEach(), if else
+
+/*
+You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return:
+
+true if at least one Ruby developer has signed up; or
+false if there will be no Ruby developers.
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Emma', lastName: 'Z.', country: 'Netherlands', continent: 'Europe', age: 29, language: 'Ruby' },
+  { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'Javascript' },
+  { firstName: 'Jayden', lastName: 'P.', country: 'Jamaica', continent: 'Americas', age: 42, language: 'JavaScript' }
+];
+your function should return true.
+
+Notes:
+
+The input array will always be valid and formatted as in the example above.
+*/
+
+function isRubyComing(list) {
+    let numOfRubyDevs = 0
+    list.forEach(e => e.language == "Ruby" ? numOfRubyDevs++ : "")
+  //   for(let i = 0; i < list.length; i++){
+  //     if(list[i].language == "Ruby"){
+  //       numOfRubyDevs++
+  //     }
+  //   }
+    if(numOfRubyDevs >= 1){
+        return true
+    }else{
+        return false
+    }
+  }
+
+// Alternative Solution
+function isRubyComing(list) {
+    return list.some(e => e.language == "Ruby")
+  }
