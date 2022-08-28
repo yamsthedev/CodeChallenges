0faac21b5930fec8ea8771ab19cc46a2461b2bc2
Sam Luong <newaccrs@gmail.com>
2022-08-28 14:48:11 -0400

    Code Challenges - August 28th 2022 - 7 Kyu Problem - Making Copies - .slice(), Array.isArray(), [...arr], Array.from(arr), [].concat(arr), arr.map(e => e)

---
+++ 7_kyu/2022-08-28-Problem.js
@@ -0,0 +1,44 @@
+// 7 Kyu Problem - August 28th 2022
+// Making Copies
+// Array.isArray(arr), .slice(), Array.from(arr), [...arr], [].concat(arr), arr.map(e => e)
+
+/*
+Write a function that takes a list (in Python) or array (in other languages) of numbers, and makes a copy of it.
+
+Note that you may have troubles if you do not return an actual copy, item by item, just a pointer or an alias for an existing list or array.
+
+If not a list or array is given as a parameter in interpreted languages, the function should raise an error.
+
+Examples:
+
+t = [1, 2, 3, 4]
+tCopy = copyList(t)
+t[1] += 5
+t = [1, 7, 3, 4]
+tCopy = [1, 2, 3, 4]
+*/
+
+function copyList(l){
+ return l.slice()
+}
+
+function copyList(l){
+  return Array.from(l)
+}
+
+function copyList(l){
+    return [...l]
+}
+
+function copyList(l){
+    return [].concat(l)
+}
+
+function copyList(l){
+    return l.map(e => e)
+}
+
+function copyList(l){
+    return Array.isArray(l) ? l.slice() : "Copy Unsuccesful"
+}
+  
