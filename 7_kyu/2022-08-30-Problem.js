b565e8b294ada00f9dc94dd6b66ef52bbb1caa87
Sam Luong <newaccrs@gmail.com>
2022-08-30 19:15:13 -0400

    Code Challenges - August 30th 2022 - 7 Kyu Problem - Basic Calculator - Switch Statement, Object values can be functions, calling for an object property that does not exist will return undefined

---
+++ 7_kyu/2022-08-30-Problem.js
@@ -0,0 +1,59 @@
+// 7 Kyu Problem - August 30th 2022
+// Basic Calculator
+// Switch Statement, objects with functions as values
+
+/*
+Write a function called calculate that takes 3 values. The first and third values are numbers. The second value is a character. If the character is "+" , "-", "*", or "/", the function will return the result of the corresponding mathematical function on the two numbers. If the string is not one of the specified characters, the function should return null (throw an ArgumentException in C#).
+
+calculate(2,"+", 4); //Should return 6
+calculate(6,"-", 1.5); //Should return 4.5
+calculate(-4,"*", 8); //Should return -32
+calculate(49,"/", -7); //Should return -7
+calculate(8,"m", 2); //Should return null
+calculate(4,"/",0) //should return null
+Keep in mind, you cannot divide by zero. If an attempt to divide by zero is made, return null (throw an ArgumentException in C#)/(None in Python).
+*/
+
+function calculate(num1, operation, num2) {
+    switch(operation)
+    {
+      case "+":
+        return num1 + num2;
+        break;
+      case "-":
+        return num1 - num2;
+        break;
+      case "*":
+        return num1*num2;
+        break;
+      case "/":
+        if(num2 === 0){return null;}else{
+        return num1 / num2;
+        }
+        break;
+      default:
+        return null;
+    }
+   }
+
+// Alternative Solution
+function calculate(num1, operation, num2) {
+    let obj = {
+      "+": (a,b) => a+b, 
+      "-": (a,b) => a-b,
+      "*": (a,b) => a*b,
+      "/": (a,b) => b === 0 ? null : a/b
+    }
+    return obj[operation] ? obj[operation](num1,num2) : null
+   }
+
+// Alternative Solution
+function calculate(num1, operation, num2) {
+    var ops = {
+      '+': function(x, y) { return x + y; },
+      '-': function(x, y) { return x - y; },
+      '*': function(x, y) { return x * y; },
+      '/': function(x, y) { return y === 0 ? null : x / y; }
+    };
+    return (ops[operation] || function() { return null; })(num1, num2);
+  }
