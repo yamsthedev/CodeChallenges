2487d667b1d636530c394912fd7fd6847808fcb1
Sam Luong <newaccrs@gmail.com>
2022-10-26 19:09:14 -0400

    Code Challenges - October 26th 2022 - 6 Kyu Problem - Sums of Parts - .unshift(), .map(), .reduce(), reassignment

---
+++ 6_Kyu/2022-10-26-Problem.js
@@ -0,0 +1,44 @@
+// 6 Kyu Problem - October 26th 2022
+// Sums of Parts
+// Math, new Array(), .map(), .reduce(), .unshift(), reassignment
+
+/*
+Let us consider this example (array written in general format):
+
+ls = [0, 1, 3, 6, 10]
+
+Its following parts:
+
+ls = [0, 1, 3, 6, 10]
+ls = [1, 3, 6, 10]
+ls = [3, 6, 10]
+ls = [6, 10]
+ls = [10]
+ls = []
+The corresponding sums are (put together in a list): [20, 20, 19, 16, 10, 0]
+
+The function parts_sums (or its variants in other languages) will take as parameter a list ls and return a list of the sums of its parts as defined above.
+
+Other Examples:
+ls = [1, 2, 3, 4, 5, 6] 
+parts_sums(ls) -> [21, 20, 18, 15, 11, 6, 0]
+
+ls = [744125, 935, 407, 454, 430, 90, 144, 6710213, 889, 810, 2579358]
+parts_sums(ls) -> [10037855, 9293730, 9292795, 9292388, 9291934, 9291504, 9291414, 9291270, 2581057, 2580168, 2579358, 0]
+*/
+
+
+function partsSums(ls){
+  ls.unshift(0)
+  let sum = ls.reduce((acc,curr) => acc + curr, 0)
+  return ls.map(e => sum = sum - e, sum)
+}
+
+function partsSums(ls){
+  let x = new Array(ls.length + 1)
+  x[ls.length] = 0
+  for(let i = ls.length - 1; i >= 0; i--){
+    x[i] = x[i+1] + ls[i]
+  }
+  return x
+}
